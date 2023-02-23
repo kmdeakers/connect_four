113670b97de5b166f2615b8de31342dc4c08b104
Huse Kivrak <huse7@pm.me>
2023-02-22 21:16:21 -0500

    placeInTable, piece CSS class, handleClick, findSpotForCol

--- connect4.js
+++ connect4.js
@@ -7,13 +7,11 @@
  * board fills (tie)
  */
 
-const WIDTH = 7; //y
-const HEIGHT = 6; //x
+const WIDTH = 7; //x
+const HEIGHT = 6; //y
 
 let currPlayer = 1; // active player: 1 or 2
 let board = []; // array of rows, each row is array of cells  (board[y][x])
-
-
 
 
 /** makeBoard: create in-JS board structure:
@@ -25,10 +23,9 @@ function makeBoard() {
   for (let y = 0; y < HEIGHT; y++){
     let row = [];
     for (let x = 0; x < WIDTH; x++){
-      let column = null;
-      row.push(column);
+      // let column = null;
+      row.push(null);
     }
-    console.log(row);
     board.push(row);
   }
   console.log(board);
@@ -39,15 +36,15 @@ function makeBoard() {
 function makeHtmlBoard() {
   const htmlBoard = document.getElementById('board');
 
-  //  create clickable drop row
+  //  create clickable row where pieces will be dropped
   const top = document.createElement("tr");
   top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
 
-  //  fill drop row with cells and add id
+  //  fill clickable row with cells and add id
   for (let x = 0; x < WIDTH; x++) {
     let headCell = document.createElement("td");
-    headCell.setAttribute("id", `top-${x}`);
+    headCell.setAttribute("id", x);
     top.append(headCell);
   }
   htmlBoard.append(top);
@@ -57,34 +54,52 @@ function makeHtmlBoard() {
   // uses WIDTH to create table cells for each row
   for (let y = 0; y < HEIGHT; y++) {
     //  Create a table row element and assign to a "row" variable
-    let row = document.createElement('tr');
-    row.setAttribute("id", `row-${y}`);
+    const row = document.createElement('tr');
+
     for (let x = 0; x < WIDTH; x++) {
       //  Create a table cell element and assign to a "cell" variable
-      let cell = document.createElement('td');
+      const cell = document.createElement('td');
       //  add an id, c-y-x, to the above table cell element
       // you'll use this later, so make sure you use c-y-x
        cell.setAttribute('id', `c-${y}-${x}`);
       //  append the table cell to the table row
-      row.appendChild(cell);
+      row.append(cell);
     }
     //  append the row to the html board
-    htmlBoard.appendChild(row);
+    htmlBoard.append(row);
   }
 }
+//!! change appendChild -> append; let row -> const row;
 
 /** findSpotForCol: given column x, return bottom empty y (null if filled) */
 
 function findSpotForCol(x) {
-  // TODO: write the real version of this, rather than always returning 5
-  return 5;
+  // write the real version of this, rather than always returning 5
+  for (let y = HEIGHT-1; y >= 0; y--){
+    if (board[y][x] === null) return y;
+  }
+  return null;
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
-  // TODO: make a div and insert into correct table cell
+  // make a div and insert into correct table cell
 
+  //declare a var assigned to a createElement(div)
+  const piece = document.createElement('div');
+  piece.classList.add('piece');
+
+  if (currPlayer === 1){
+    piece.classList.add('p1');
+  } else {
+    piece.classList.add('p2');
+  }
+
+  const pieceSlot = document.getElementById(`c-${y}-${x}`);
+  console.log(`c-${y}-${x}`)
+  console.log(pieceSlot)
+  pieceSlot.append(piece);
 }
 
 /** endGame: announce game end */
@@ -98,6 +113,7 @@ function endGame(msg) {
 function handleClick(evt) {
   // get x from ID of clicked cell
   const x = +evt.target.id;
+  console.log("x=", x);
 
   // get next spot in column (if none, ignore click)
   const y = findSpotForCol(x);
@@ -106,8 +122,12 @@ function handleClick(evt) {
   }
 
   // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
   placeInTable(y, x);
+
+
+  // add line to update in-memory board
+  board[y][x] = currPlayer;
+  console.log(board);
 
   // check for win
   if (checkForWin()) {
@@ -115,10 +135,15 @@ function handleClick(evt) {
   }
 
   // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
+  // check if all cells in board are filled; if so call, call endGame
+   if (board.every ((row) => !row.includes(null))){
+    endGame();
+    }
+
 
   // switch players
-  // TODO: switch currPlayer 1 <-> 2
+  currPlayer === 1 ? currPlayer = 2 : currPlayer = 1;
+  console.log("currPlayer = p", currPlayer);
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
